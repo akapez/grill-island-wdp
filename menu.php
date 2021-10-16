20ae67041d54cab7aea7ad3dbabf06735e7266f3
avishka964 <avishkakapuruge@gmail.com>
2021-10-16 20:43:10 +0530

    search fixed

--- menu.php
+++ menu.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'components/header.php';
-require_once 'config/DatabaseConn2.php';
-$result =  $menu->getMenuData();
+require_once 'config/DatabaseConn1.php';
 
 ?>
 
@@ -10,33 +9,49 @@ $result =  $menu->getMenuData();
 
 <!--search option-->
 <div class="search_container">
-  <form action="/action_page.php" autocomplete="off">
+  <form action="search.php" autocomplete="off" method="POST">
     <input class="search_input" type="text" placeholder="Search.." name="search" />
-    <button class="search_button" type="submit">Search</button>
+    <button class="search_button" type="submit" name="submit-search">Search</button>
   </form>
 </div>
 
 <!--menu container-->
 <div class="menu_container">
-  <?php while ($r = $result->fetch(PDO::FETCH_ASSOC)) { ?>
-    <form action="cart_manage.php" method="POST">
-      <div class="menu">
-        <img class="menu-img" src="<?php echo $r['foodImage'] ?>" alt="menu-item" />
-        <h4><?php echo $r['foodName'] ?></h4>
-        <h5 style="color: #4A403A"><?php echo $r['variety'] ?></h5>
-        <h5 style="color: #A2416B"><?php echo $r['size'] ?></h5>
-        <p>RS. <?php echo $r['price'] ?></p>       
-        <div>
-          <button class="order_button" name="addToCart" type="submit">ADD TO CART</button>  
-          <input type="hidden" name="foodName" value="<?php echo $r['foodName'] ?>"/>
-          <input type="hidden" name="variety" value="<?php echo $r['variety'] ?>"/>
-          <input type="hidden" name="size" value="<?php echo $r['size'] ?>"/>
-          <input type="hidden" name="price" value="<?php echo $r['price'] ?>"/>         
-          <input type="hidden" name="foodImage" value="<?php echo $r['foodImage'] ?>"/> 
-        </div>
-      </div>
-    </form>
-  <?php } ?>
+  <?php 
+      $sql = "SELECT * FROM menus";
+      $res = mysqli_query($connection, $sql);    
+      $query_result = mysqli_num_rows($res);
+
+      if($query_result >0){
+        while ($row = mysqli_fetch_assoc($res)) {
+          echo "
+          <form action='cart_manage.php' method='POST'>
+          <div class='menu'>
+            <img class='menu-img' src='$row[foodImage]' alt='menu-item' />
+            <h4>$row[foodName]</h4>
+            <h5 style='color: #4A403A'>$row[variety]</h5>
+            <h5 style='color: #A2416B'>$row[size]</h5>
+            <p>RS. $row[price]</p>       
+            <div>
+              <button class='order_button' name='addToCart' type='submit'>ADD TO CART</button>  
+              <input type='hidden' name='foodName' value='$row[foodName]' />
+              <input type='hidden' name='variety' value='$row[variety]'  />
+              <input type='hidden' name='size' value='$row[size]' />
+              <input type='hidden' name='price' value='$row[price]' />         
+              <input type='hidden' name='foodImage' value='$row[foodImage]'/> 
+            </div>
+          </div>
+        </form>
+          
+          ";
+
+        }
+      }
+
+
+  ?>
+
+
 </div>
 
 
